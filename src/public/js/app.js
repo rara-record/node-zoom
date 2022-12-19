34a0fff6bb48302fc63d4ac754d1f77d3871e27d
BORA <a52447879@gmail.com>
2022-12-19 23:16:51 +0900

    feat: chat message 보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,24 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 
 socket.addEventListener('open', () => {
   console.log('connected to server ✔')
 })
 
 socket.addEventListener('message', (message)=> {
-  console.log(message, 'from to the server')
+  console.log(message.data, 'from to the server')
 })
 
 socket.addEventListener("close",  () => {
   console.log('connected from server ❌')
 })
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener('submit', handleSubmit)
