f05b0f49682de0c83bc07c576e199e9d55007609
BORA <a52447879@gmail.com>
2022-12-19 22:47:58 +0900

    fix: 버전 문제로 인한 웹소켓 연결 방식 수정
    new WebSocket.server => new WebSocketServer

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from 'http'
 import express from "express";
-
+import {WebSocketServer} from "ws";
 const app = express();
 
 app.set("view engine", "pug");
@@ -10,8 +10,8 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
 
 
 wss.on("connection", (socket) => {
