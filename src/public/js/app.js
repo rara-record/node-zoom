60fe8dc38c23ba4361dfd2f69e7b45e7959068c4
BORA <a52447879@gmail.com>
2022-12-21 23:39:55 +0900

    feat: 타입별로 메세지를 구분하여 데이터를 보낸다.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,14 @@ const messageList = document.querySelector("ul");
 const nickForm = document.querySelector("#nick");
 const messageForm = document.querySelector('#message');
 
+const makeMessage = (type, payload) => {
+  const message = { type, payload };
+  return JSON.stringify(message);
+}
+
 socket.addEventListener('open', () => {
   console.log('connected to server ✔')
 })
-
 
 socket.addEventListener("message", (message) => {
   console.log('new message', message.data)
@@ -18,8 +22,16 @@ socket.addEventListener("message", (message) => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
   input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
